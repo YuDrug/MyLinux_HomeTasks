16d5d8f299cfcadfce233a81564aa84f5780bfb4
YuDrug <141164135+YuDrug@users.noreply.github.com>
2023-09-01 22:00:23 +0300

    my Testscript file -
    Linux HomeTask Lesson 4 - date 28AUG23

---
+++ testscript.sh
@@ -0,0 +1,27 @@
+  GNU nano 2.9.8                                 testscript.sh
+
+#!/bin/bash
+
+        VAR1=ec2-user@/yurii
+        VAR2=/opt/240723_evening/yurii
+        VAR3=TopTasks_107_on_Server_Today
+
+echo " "
+echo "Hello everybody!"
+echo " "
+echo "Your name is: " $VAR1
+echo " "
+echo "Yuor working directory is :" $VAR2
+echo " "
+echo "Number of users:" $VAR3
+echo " "
+date
+echo "Glory to Ukraine!"
+echo "DONE"
+
+
+
+
+
+
+
